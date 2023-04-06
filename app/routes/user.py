e44e045f5b30ac6543871bef405e11b29e7e92f5
Andrey <af7slr722@gmail.com>
2023-04-06 14:48:07 +0000

    added requirements

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,10 +1,9 @@
 from flask import Blueprint
 
-
 bp_user = Blueprint('user', __name__)
 
 
 @bp_user.route('/')
 def reg_user():
-    new_user = User('valera666', '123', 'a@1', '3', True, )
-    return 'user created'
+    
+    return ''
